c55eea26d292fa21d6c016353783e944314b2951
David Moya M <dav.moyam@gmail.com>
2021-02-25 18:18:48 -0300

    save

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiKeyGif: "bbLU7Kp1cbIFKx7XmnqPPgbmj8Bu3PsV"
+  apiKeyGif: "bbLU7Kp1cbIFKx7XmnqPPgbmj8Bu3PsV",
+  urlGiphyApi: "https://api.giphy.com/v1/gifs/"
 };
